15d4caec1c38e2f5677444053b6588fce84d3c71
Matteo Pacini <m+github@matteopacini.me>
2024-08-18 09:55:42 +0100

    opensupaplex: move to by-name

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -23151,8 +23151,6 @@ with pkgs;
 
   opensubdiv = callPackage ../development/libraries/opensubdiv { };
 
-  opensupaplex = callPackage ../games/opensupaplex { };
-
   open-wbo = callPackage ../applications/science/logic/open-wbo { };
 
   openwsman = callPackage ../development/libraries/openwsman { };
